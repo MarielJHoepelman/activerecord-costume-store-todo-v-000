d85be6559a0635b67110a5003e5ef73d85f6714e
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-19 23:58:41 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,3 +12,11 @@
 #   2. price
 #   3. size
 #   4. image url
+class CreateDogs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+end
