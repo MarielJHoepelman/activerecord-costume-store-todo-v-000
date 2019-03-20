10c5102257b8643977d2838afd736ab545cfd8e7
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-20 00:27:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@
 #  6. opening time
 #  7. closing time
 
-class CreateCostumeStores < ActiveRecord::Migration[5.1]
+class CreateCostumeStore < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_store do |t|
       t.string :name
