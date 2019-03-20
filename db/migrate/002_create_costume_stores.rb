eddfe8f4e242ce676ff7e36ba26a9258d1d685ae
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-20 00:35:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -18,8 +18,6 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.boolean :still_in_business
       t.string :opening_time
       t.string :closing_time
-
-      #t.timestamps
     end
   end
 end
