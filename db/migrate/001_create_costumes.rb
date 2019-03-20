7a5d5d32c2e4acc86e94d29ea3da0cbe4e4c9ed7
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-20 00:12:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -19,6 +19,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size
       t.string :image_url
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
