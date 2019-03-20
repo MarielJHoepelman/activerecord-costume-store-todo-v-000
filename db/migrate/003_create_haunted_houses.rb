77acfbd79d14d58ce2388b7d2c8c276780d236b6
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-20 00:42:45 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,17 +9,17 @@
 #  7. closing date
 #  8. long description
 
-#class CreateHauntedHouses < ActiveRecord::Migration[5.1]
-#  def change
-#    create_table :haunted_houses do |t|
-#      t.string :name
-#      t.string :location
-#      t.string :theme
-#      t.float :price
-#      t.boolean :family_friendly
-#      t.string :opening_date
-#      t.string :closing_date
-#      t.string :description
-#    end
-#  end
-#end
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.string :opening_date
+      t.string :closing_date
+      t.string :description
+    end
+  end
+end
